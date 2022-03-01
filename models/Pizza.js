02391b8b524142775691351faa217d6fdbbf314a
Blake Georgii <blakegeorgii@gmail.com>
2022-03-01 15:08:54 -0800

    added comment count virtual to pizza model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,28 +1,40 @@
 const { Schema, model } = require('mongoose');
 
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: [],
-    comments: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Comment'
-          }
-      ]
-});
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+PizzaSchema.virtual('commentCount').get(() => {
+    return this.comments.length;
+})
 
 const Pizza = model('Pizza', PizzaSchema);
 
